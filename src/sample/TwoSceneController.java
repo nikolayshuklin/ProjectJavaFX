79714ce596d5dbec2565d9831bda4a3c4119d177
nikolayshuklin <nikolayshuklin@mail.ru>
2022-07-25 15:56:00 +0300

    Переделал под кулинарную книгу. Перезалил

---
+++ src/sample/TwoSceneController.java
@@ -0,0 +1,17 @@
+package sample;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+
+public class TwoSceneController {
+
+    @FXML
+    private ScrollPane scrollPane;
+
+    @FXML
+    private Label labelScroll;
+
+
+}
